b46251142aeb91037ebb8f464361ce04365ad5ae
Abhishek Tanwer <abhishektanwer2@gmail.com>
2021-02-25 17:12:58 -0500

    Map Based Service Closes #6

--- spring-petclinic-data/src/main/java/com/example/services/CrudService.java
+++ spring-petclinic-data/src/main/java/com/example/services/CrudService.java
@@ -7,6 +7,6 @@ public interface CrudService <T,ID>{
     T findById(ID id);
     T save(T object);
     void delete(T object);
-    void deleteById(T object);
+    void deleteById(ID object);
 
 }
---
+++ spring-petclinic-data/src/main/java/com/example/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.example.services.map;
+
+import com.example.model.Vet;
+import com.example.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet,Long> implements CrudService<Vet,Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long object) {
+super.deleteById(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
